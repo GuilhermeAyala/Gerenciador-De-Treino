b841233bc15b71af47e4fd1662768746e0b6b538
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2026-02-11 23:27:53 -0300

    Add POST e GET
    Requisição ok, tente de json, add post do exercicio e puxar uma array do exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -1,7 +1,22 @@
-export class Exercicio {
-    ItensExercicios = [];
+import { randomUUID } from 'crypto';
 
-    create(){
-        this.ItensExercicios.push(ItensExercicios);
+export class BancoDeExercicios{
+    exercicios = new Map();
+
+    list(){
+        return Array.from(this.exercicios.values());
+    }
+
+    create(exercicio){
+        const exercicioId = randomUUID();
+        this.exercicios.set(exercicioId, exercicio);
+    }
+
+    update(id, exercicio){
+        this.exercicio.set(id, exercicio);
+    }
+
+    delete(id){
+        this.exercicios.delete(id);
     }
 }
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-import { Exercicio } from 'exercicio.js'
+import { BancoDeExercicios } from './exercicio.js';
+import express from 'express';
 
-const express = require('express')
 const app = express()
 const port = 3000
+
+const database = new BancoDeExercicios();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Teste")
@@ -11,6 +15,27 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
     res.send("Você está na Home")
 })
+
+app.post('/exercicios', (req, res) => {
+    const {nome, quantidade_series, quantidade_repeticao, peso_carga} = req.body;
+
+    database.create({
+        nome,
+        quantidade_series,
+        quantidade_repeticao,
+        peso_carga,
+    })
+
+    return res.status(201).send();
+})
+
+app.get('/exercicios', (req, res) => {
+    const exercicios = database.list();
+
+    //return exercicios;
+    return res.json(exercicios);
+})
+
 
 app.listen(port, () => {
     console.log(`Acessando a porta ${port}`);
