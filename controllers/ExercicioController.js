26f42dfa3ba8c1080cef1f91655024bd3b3209ec
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2026-02-18 23:33:46 -0300

    verificação exercicios
    Verificação no controller de exercícios

--- controllers/ExercicioController.js
+++ controllers/ExercicioController.js
@@ -6,6 +6,22 @@ function listarExercicios(req, res) {
 
 function adicionarExercicios(req, res) {
     const {nome, quantidade_series, quantidade_repeticao, peso_carga} = req.body;
+    if(!nome || !quantidade_series || !quantidade_repeticao || !peso_carga){
+        return res.status(400).json({message: "Todos os campos são obrigatórios"})
+    }
+
+    if(typeof nome !== "string"){
+        return res.status(400).json({error: "O exercicio precisa ser uma string"})
+    }
+
+    if(typeof quantidade_series !== "number" || typeof quantidade_repeticao !== "number" || typeof peso_carga !== "number"){
+        return res.status(400).json({error: "Esses campos devem ser numeros"})
+    }
+
+    if(quantidade_series <= 0 || quantidade_repeticao <= 0 || peso_carga <= 0){
+        return res.status(400).json({error: "Series, repetições ou cargas, não podem ser nulas ou negativas"})
+    }
+
     const novo = new Exercicio(nome, quantidade_series, quantidade_repeticao, peso_carga);
     Exercicio.adicionar(novo);
     res.status(201).json({message: "Exercicio cadastrado com sucesso"});
